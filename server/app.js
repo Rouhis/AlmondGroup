fdb7a70ec07d02000d2fe100174067f55989e9de
majim3 <45204846+majim3@users.noreply.github.com>
2022-12-03 12:24:26 +0200

    fixes

--- server/app.js
+++ server/app.js
@@ -6,11 +6,12 @@ const userRouter = require("./routes/userRoute");
 
 const port = 3000;
 
-
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended:true}));
 app.use("/user",userRouter);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.listen(port, () => console.log(`app listening on port ${port}!`))
+
