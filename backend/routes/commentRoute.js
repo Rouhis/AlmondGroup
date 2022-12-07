0ac854452b62058849c8316e45e61b1a1cf73328
AxelKah <axel.kahkonen@gmail.com>
2022-12-07 09:21:28 +0200

    push

--- backend/routes/commentRoute.js
+++ backend/routes/commentRoute.js
@@ -2,21 +2,8 @@
 
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
 const {body} = require("express-validator");
 const commentController = require("../controllers/commentController");
-
-
-const fileFilter = (req, file, cb) => {
-    const acceptedTypes = ["image/jpeg","image/png","image/gif"];
-    if (acceptedTypes.includes(file.mimetype)){
-        cb(null,true);
-    }else{
-        cb(null,false);
-    }
-};
-
-const upload = multer({dest: "uploads/", fileFilter});
 
 
 router.get("/",commentController.getComments)
