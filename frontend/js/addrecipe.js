a7f8a620bd1b140aa46d6df7a7ab806e92c55699
Rouhis <eeturouhiainen1@gmail.com>
2022-12-08 10:25:19 +0200

    comment field to recipe page

--- frontend/js/addrecipe.js
+++
@@ -1,7 +0,0 @@
-function (event) {
-    var output = document.getElementById('output');
-    output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function () {
-        URL.revokeObjectURL(output.src) 
-    }
-};
