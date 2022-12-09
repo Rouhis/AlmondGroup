4f1d136d5ebbdfb63a45342944eeb4d2bd03d7ee
Rouhis <eeturouhiainen1@gmail.com>
2022-12-09 13:30:33 +0200

    recipe add template

--- frontend/js/addrecipe.js
+++ frontend/js/addrecipe.js
@@ -5,22 +5,3 @@ let loadFile = function (event) {
         URL.revokeObjectURL(output.src) 
     }
 };
-
-let counter = 0;
-
-function addField(){
-    counter++;
-    let newFields = document.getElementById(textfield).cloneNode(true);
-    newFields.id = '';
-    newFields.style.display = 'block';
-    let newField = newFields.childNodes;
-    for (let i = 0; i < newField.length; i++){
-        let thename = newField[i].nodeName;
-        if (thename){
-            newField[i].name = theName + counter;
-        }
-    
-    }
-    let insert = document.getElementById(add_ingredients);
-    insertHere.parentNode.insertBefore(newFields,insertHere);
-}
