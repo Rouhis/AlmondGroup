0e34690a982314eb0cf418e34bf1c50a81fa5d45
majim3 <45204846+majim3@users.noreply.github.com>
2022-12-09 14:10:27 +0200

    null ongelma

--- frontend/js/singin.js
+++ frontend/js/singin.js
@@ -1,28 +1,23 @@
 'use strict';
 const url = 'http://localhost:3000'; // change url when uploading to server
 
+
 const loginForm = document.querySelector('#login_form');
 
+
 loginForm.addEventListener('submit', async (evt) => {
-    evt.preventDefault();
-    const data = serializeJson(loginForm);
-    const fetchOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    };
-  
-    const response = await fetch(url + '/auth/login', fetchOptions);
-    const json = await response.json();
-    if (!json.user) {
-      alert(json.error.message);
-    } else {
-      // save token and user
-      sessionStorage.setItem('token', json.token);
-      sessionStorage.setItem('user', JSON.stringify(json.user));
-      user = JSON.parse(sessionStorage.getItem('user'));
-      startApp(true);
-    }
-  });
+  evt.preventDefault();
+  const data = serializeJson(loginForm);
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  };
+    console.log(fetchOptions)
+  const response = await fetch(url + '/auth/login', fetchOptions);
+  const json = await response.json();
+  alert(json.message);
+  console.log(json);
+});
